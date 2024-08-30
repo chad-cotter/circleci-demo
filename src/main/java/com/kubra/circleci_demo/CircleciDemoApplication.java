b42faed584f07eec19bb5956be054177dd5360d4
Chad Cotter <chad.cotter@kubra.com>
2024-08-30 13:03:23 -0700

    initialize project

---
+++ src/main/java/com/kubra/circleci_demo/CircleciDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kubra.circleci_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleciDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleciDemoApplication.class, args);
+	}
+
+}
